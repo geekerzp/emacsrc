706a711b0d98e6a0f2a31166c2801635fd4a2ad8
geekerzp <geekerzp@gmail.com>
2015-05-05 22:21:23 +0800

    update flycheck config file super-flycheck

--- elpa/haskell-mode-20150502.1111/haskell-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "haskell-mode" "20150502.1111" "A Haskell editing mode"
-  '((cl-lib "0.5")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- modules/super-flycheck.el
+++ modules/super-flycheck.el
@@ -1,5 +1,16 @@
-;; Code:
+;;; super-flycheck --- flycheck config
+
+;;; Commentary:
+
+;;; Code:
+(require 'flycheck)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;; checker for python mode
+(add-hook 'python-mode-hook
+          #'(lambda ()
+              (flycheck-select-checker 'python-flake8)))
+
 (provide 'super-flycheck)
+;;; super-flycheck ends here
