273c3f9a5e4debb1bc85e994e930715d1cae7164
geekerzp <geekerzp@gmail.com>
2015-10-12 20:51:39 +0800

    remove elpa folder

--- elpa/dash-20151010.929/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22043 7245 233349 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- elpa/ede-compdb-20150920.1333/ede-compdb-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ede-compdb" "20150920.1333" "Support for compilation database projects in EDE" '((ede "1.2") (semantic "2.2") (cl-lib "0.4")) :keywords '("development" "ninja" "build" "cedet" "ede"))
