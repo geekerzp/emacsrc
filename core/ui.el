6fcbf8b2d08617751e7b12beecca230fcc9a2d88
geekerzp <geekerzp@gmail.com>
2015-11-07 12:59:49 +0800

    update

--- core/ui.el
+++ core/ui.el
@@ -12,7 +12,7 @@
                     :family "Envy Code R" :height 110 :weight 'light)
 
 ;; theme
-(load-theme 'distinguished t)
+(load-theme 'leuven t)
 
 ;; mode line
 (line-number-mode t)
@@ -22,7 +22,7 @@
 ;; smart mode line
 (require 'smart-mode-line)
 (setq sml/no-confirm-load-theme t)
-(setq sml/theme 'respectful)
+(setq sml/theme 'light)
 (add-hook 'after-init-hook #'sml/setup)
 
 ;; nice scrolling
