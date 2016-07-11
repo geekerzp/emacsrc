01113ebf2619d30556b076e4b60bd01e5fa30ef5
geekerzp <geekerzp@gmail.com>
2016-07-11 11:46:39 +0800

    update web-mode indent two spaces

--- langs/kitten-web.el
+++ langs/kitten-web.el
@@ -23,6 +23,10 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 (setq web-mode-attr-indent-offset 2)
 (setq web-mode-comment-style 2)
 
