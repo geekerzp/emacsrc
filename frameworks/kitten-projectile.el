28ab43bb879cfa2b6001fb918b473c20f855c2e3
geekerzp <geekerzp@gmail.com>
2016-10-11 21:12:58 +0800

    remove projectile-rails

--- frameworks/kitten-projectile.el
+++ frameworks/kitten-projectile.el
@@ -3,22 +3,19 @@
 ;;; Commentary:
 
 ;;; Code:
-(kitten/require-packages '(projectile projectile-rails))
+(kitten/require-packages '(projectile))
 
 (require 'projectile)
-(require 'projectile-rails)
 
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'helm)
 
 (defun kitten/projectile-mode-hook ()
   "Hooks for Projectile."
-  (projectile-rails-on)
   (helm-projectile-on))
 
-(add-hook 'projectile-mode-hook #'kitten/projectile-mode-hook)
+(add-hook 'projectile-mode-hook 'kitten/projectile-mode-hook)
 
-;; Enable Projectile globally
 (projectile-global-mode)
 
 (provide 'kitten-projectile)
