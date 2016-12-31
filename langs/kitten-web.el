07ab2f8723a8b194f05e1d8549c32535f6631cb7
geekerzp <geekerzp@gmail.com>
2016-12-31 16:27:48 +0800

    ip web-mode

--- langs/kitten-web.el
+++ langs/kitten-web.el
@@ -19,6 +19,7 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
