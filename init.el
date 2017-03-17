d80ae0c4c777844929a955b26236091e8f633466
geekerzp <geekerzp@gmail.com>
2017-03-17 23:20:22 +0800

    Remove icons

--- init.el
+++ init.el
@@ -91,21 +91,16 @@
  '(anzu-mode-lighter "")
  '(anzu-replace-to-string-separator " => ")
  '(anzu-search-threshold 1000)
- '(custom-safe-themes
-   (quote
-    ("14f0fbf6f7851bfa60bf1f30347003e2348bf7a1005570fd758133c87dafe08f" "21ebd14a266a6fe91980a5493a411cf6b7f27fd62da2bada897361ab7c4b2a98" default)))
  '(livedown-autostart nil)
  '(livedown-browser nil)
  '(livedown-open t)
  '(livedown-port 1337)
  '(package-selected-packages
    (quote
-    (neotree all-the-icons ranger robe php-mode alchemist csv-mode elixir-mode scss-mode darktooth-theme yaml-mode slim-mode emmet-mode web-beautify yasnippet yari web-mode volatile-highlights undo-tree sr-speedbar smooth-scrolling smartrep smart-mode-line sly-company ruby-tools ruby-end rspec-mode rbenv rainbow-mode rainbow-delimiters pyenv-mode paredit ov operate-on-number nyan-mode move-text magit key-chord json-mode js2-mode irony indent-guide highlight-symbol helm-projectile helm-descbinds helm-ag grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist findr fic-mode expand-region exec-path-from-shell elisp-slime-nav easy-kill distinguished-theme discover-my-major dired+ diminish diff-hl company-tern company-anaconda coffee-mode cider browse-kill-ring avy auto-compile anzu ag))))
+    (yasnippet yari yaml-mode web-mode web-beautify volatile-highlights undo-tree typescript-mode smooth-scrolling smartrep smart-mode-line sly-company slim-mode scss-mode ruby-tools ruby-end rspec-mode robe rbenv ranger rainbow-mode rainbow-delimiters pyenv-mode paredit ov operate-on-number nyan-mode neotree move-text magit key-chord json-mode js2-mode irony indent-guide highlight-symbol helm-projectile helm-descbinds helm-ag grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck findr fic-mode expand-region exec-path-from-shell emmet-mode elisp-slime-nav easy-kill distinguished-theme discover-my-major dired+ diminish diff-hl darktooth-theme csv-mode company-tern company-anaconda coffee-mode cider browse-kill-ring avy auto-compile anzu all-the-icons alchemist ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
