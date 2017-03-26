50f8dad81dff793068ce211542f51e8ba2451dfa
geekerzp <geekerzp@gmail.com>
2017-03-26 21:47:13 +0800

    Update web-mode

--- langs/ghost-web.el
+++ langs/ghost-web.el
@@ -15,6 +15,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
 (setq web-mode-engines-alist
       '(("django"    . "\\.html?\\'")))
