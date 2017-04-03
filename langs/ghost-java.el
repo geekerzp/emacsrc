344e699e98b12b9f7a590f06077338e3c0160e2d
geekerzp <geekerzp@gmail.com>
2017-04-03 22:53:23 +0800

    Update java mode

--- langs/ghost-java.el
+++ langs/ghost-java.el
@@ -4,6 +4,7 @@
 
 (defun ghost-java-mode-hook ()
   (meghanada-mode t)
+  (rainbow-delimiters-mode t)
   (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))
 
 (add-hook 'java-mode-hook 'ghost-java-mode-hook)
