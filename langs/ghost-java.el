9212d2812eef8c85d32fb34cb2bbe4f0699cbbe7
geekerzp <geekerzp@gmail.com>
2017-05-19 17:57:13 +0800

    Fix indention

--- langs/ghost-java.el
+++ langs/ghost-java.el
@@ -7,6 +7,7 @@
 (defun ghost-java-mode-hook ()
   (meghanada-mode t)
   (rainbow-delimiters-mode t)
+  (setq c-basic-offset 2)
   (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))
 
 (add-hook 'java-mode-hook 'ghost-java-mode-hook)
