9d923c6e4f1061d96b201d83ac95b52d6c30a6fb
geekerzp <geekerzp@gmail.com>
2017-06-18 21:40:23 +0800

    ghost-java with use-package

--- langs/ghost-java.el
+++ langs/ghost-java.el
@@ -1,15 +1,44 @@
 ;;; Java
 
-(ghost-require-packages '(meghanada))
+(use-package autodisass-java-bytecode
+  :ensure t
+  :defer t)
 
-(require 'meghanada)
+(use-package google-c-style
+  :defer t
+  :ensure t
+  :commands
+  (google-set-c-style))
 
-(defun ghost-java-mode-hook ()
-  (meghanada-mode t)
-  (rainbow-delimiters-mode t)
+(use-package meghanada
+  :defer t
+  :init
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (google-set-c-style)
+              (google-make-newline-indent)
+              (meghanada-mode t)
+              (rainbow-delimiters-mode t)
+              (highlight-symbol-mode t)
+              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+
+  :config
+  (use-package realgud
+    :ensure t)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
   (setq c-basic-offset 2)
-  (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))
+  (setq meghanada-server-remote-debug t)
+  (setq meghanada-javac-xlint "-Xlint:all,-processing")
 
-(add-hook 'java-mode-hook 'ghost-java-mode-hook)
+  :bind
+  (:map meghanada-mode-map
+        ("C-S-t" . meghanada-switch-testcase)
+        ("M-RET" . meghanada-local-variable)
+        ("C-M-." . helm-imenu))
+
+
+  :commands
+  (meghanada-mode))
 
 (provide 'ghost-java)
