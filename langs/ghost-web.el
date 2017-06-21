a272bdf82c962e03d1dc2aaeadcdfafd094d4936
geekerzp <geekerzp@gmail.com>
2017-06-21 20:16:10 +0800

    use-package to refactor web-mode

--- langs/ghost-web.el
+++ langs/ghost-web.el
@@ -1,10 +1,26 @@
 ;;; WebTemplates
 
-(ghost-require-packages '(web-mode web-beautify emmet-mode))
+(use-package emmet-mode
+  :ensure t
+  :defer t)
 
-(require 'web-mode)
-(require 'emmet-mode)
-(require 'tern)
+(use-package web-mode
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (emmet-mode t)))
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  (setq web-mode-comment-style 2))
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -16,25 +32,5 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-
-(setq web-mode-engines-alist
-      '(("django"    . "\\.html?\\'")))
-
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-attr-indent-offset 2)
-(setq web-mode-comment-style 2)
-
-(defun ghost-web-mode-hook ()
-  "Hooks for web mode."
-  (emmet-mode 1)
-  (tern-mode 1))
-
-(add-hook 'web-mode-hook 'ghost-web-mode-hook)
 
 (provide 'ghost-web)
