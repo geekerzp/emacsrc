fd452fcb8a6c74e949bf5de58d742546f0d9de3a
geekerzp <geekerzp@gmail.com>
2017-06-28 18:21:02 +0800

    use-package to refactor flycheck

--- frameworks/ghost-flycheck.el
+++ frameworks/ghost-flycheck.el
@@ -1,21 +1,17 @@
 ;;; flycheck
 
-;;; Code:
-(ghost-require-packages '(flycheck))
-
-(require 'flycheck)
-
-;; disable emacs lisp checker
-(setq-default flycheck-disabled-checkers '(emacs-lisp emacs-lisp-checkdoc))
-
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; checker for python mode
-(when (derived-mode-p 'python-mode)
-  (flycheck-select-checker 'python-flake8))
-
-;; checker for ruby code
-(when (derived-mode-p 'ruby-mode)
-  (flycheck-select-checker 'rubocop))
+(use-package flycheck
+  :ensure t
+  :init
+  ;; disable emacs lisp checker
+  (setq-default flycheck-disabled-checkers
+                '(emacs-lisp emacs-lisp-checkdoc))
+  ;; checker for python mode
+  (when (derived-mode-p 'python-mode)
+    (flycheck-select-checker 'python-flake8))
+  ;; checker for ruby code
+  (when (derived-mode-p 'ruby-mode)
+    (flycheck-select-checker 'rubocop))
+  (global-flycheck-mode))
 
 (provide 'ghost-flycheck)
