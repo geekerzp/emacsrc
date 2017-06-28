0ca557d4be474914dec29f5cb48e904bd7b86a85
geekerzp <geekerzp@gmail.com>
2017-06-28 18:01:51 +0800

    use-package to refactor ag

--- frameworks/ghost-ag.el
+++ frameworks/ghost-ag.el
@@ -1,11 +1,6 @@
 ;;; Ag
 
-;;; Code:
-(ghost-require-packages '(ag))
-
-(require 'ag)
-
-;; ;; keybindings
-(global-set-key (kbd "C-c s") 'ag)
+(use-package ag
+  :ensure t :bind ("C-c s" . ag))
 
 (provide 'ghost-ag)
