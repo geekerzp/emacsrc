25f47c17de0de425a918f7079a5743d5c7c5ea75
geekerzp <geekerzp@gmail.com>
2017-06-29 22:13:07 +0800

    use-package to refactor key-chord

--- frameworks/ghost-key-chord.el
+++ frameworks/ghost-key-chord.el
@@ -1,24 +1,23 @@
 ;;; Key-chord
 
-(ghost-require-packages '(key-chord))
+(use-package key-chord
+  :ensure t
+  :init
+  ;; Press <jj> quickly to jump to the beginning of a visible word.
+  (key-chord-define-global "jj" 'avy-goto-word-1)
+  ;; Press <jl> quickly to jump to a visible line.
+  (key-chord-define-global "jl" 'avy-goto-line)
+  ;; Press <jk> quickly to jump to a visible character.
+  (key-chord-define-global "jk" 'avy-goto-char)
+  ;; Press <JJ> quickly to switch to previous buffer.
+  (key-chord-define-global "JJ" 'ghost-switch-to-previous-buffer)
+  ;; Press <uu> quickly to visualize the undo tree.
+  (key-chord-define-global "uu" 'undo-tree-visualize)
+  ;; Press <xx> quickly to execute extended command.
+  (key-chord-define-global "xx" 'execute-extended-command)
+  ;; Press <yy> quickly to browse the kill ring.
+  (key-chord-define-global "yy" 'browse-kill-ring)
 
-(require 'key-chord)
-
-;; Press <jj> quickly to jump to the beginning of a visible word.
-;; Press <jl> quickly to jump to a visible line.
-;; Press <jk> quickly to jump to a visible character.
-;; Press <JJ> quickly to switch to previous buffer.
-;; Press <uu> quickly to visualize the undo tree.
-;; Press <xx> quickly to execute extended command.
-;; Press <yy> quickly to browse the kill ring.
-(key-chord-define-global "jj" 'avy-goto-word-1)
-(key-chord-define-global "jl" 'avy-goto-line)
-(key-chord-define-global "jk" 'avy-goto-char)
-(key-chord-define-global "JJ" 'ghost-switch-to-previous-buffer)
-(key-chord-define-global "uu" 'undo-tree-visualize)
-(key-chord-define-global "xx" 'execute-extended-command)
-(key-chord-define-global "yy" 'browse-kill-ring)
-
-(key-chord-mode 1)
+  (key-chord-mode t))
 
 (provide 'ghost-key-chord)
