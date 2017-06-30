1d9ebb054fbcf1dca7ee9cad0a12cec7fc3d87d4
geekerzp <geekerzp@gmail.com>
2017-06-30 23:01:50 +0800

    use-package to refactor elixir

--- langs/ghost-elixir.el
+++ langs/ghost-elixir.el
@@ -1,23 +1,22 @@
 ;;; elixir
 
-(ghost-require-packages '(elixir-mode alchemist))
+(use-package alchemist
+  :ensure t
+  :config
+  (setq alchemist-hooks-compile-on-save t))
 
-(setq alchemist-hooks-compile-on-save t)
-
-(defun auto-activate-ruby-end-mode-for-elixir-mode ()
+(use-package elixir-mode
+  :ensure t
+  :init
+  (add-hook 'elixir-mode-hook
+            (lambda ()
+              (ruby-end-mode t)
+              (alchemist-mode t)
+              (rainbow-delimiters-mode t)))
+  :config
   (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
        "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-  (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-  (ruby-end-mode +1))
-
-(defun auto-activate-alchemist-mode-for-elixir-mode ()
-  (alchemist-mode +1))
-
-(defun auto-activate-rainbow-delimiters-mode ()
-  (rainbow-delimiters-mode 1))
-
-(add-hook 'elixir-mode-hook 'auto-activate-ruby-end-mode-for-elixir-mode)
-(add-hook 'elixir-mode-hook 'auto-activate-alchemist-mode-for-elixir-mode)
-(add-hook 'elixir-mode-hook 'auto-activate-rainbow-delimiters-mode)
+  (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers)
+       nil))
 
 (provide 'ghost-elixir)
