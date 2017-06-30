c6ab9d43bf1c29e2fba8b1016adb42dec1cfc278
geekerzp <geekerzp@gmail.com>
2017-06-30 22:26:11 +0800

    Refactor elisp

--- langs/ghost-elisp.el
+++ langs/ghost-elisp.el
@@ -1,10 +1,8 @@
 ;;; EmacsLisp
 
-(defun ghost-emacs-lisp-mode-hook ()
-  "Hooks for Emacs Lisp mode."
-  (rainbow-delimiters-mode 1)
-  (enable-paredit-mode))
-
-(add-hook 'emacs-lisp-mode-hook 'ghost-emacs-lisp-mode-hook)
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (rainbow-delimiters-mode t)
+            (enable-paredit-mode)))
 
 (provide 'ghost-elisp)
