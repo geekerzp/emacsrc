9278cd64b6f7d46421b021c69ae8cc34ecf1e419
geekerzp <geekerzp@gmail.com>
2017-09-11 20:37:53 +0800

    Update

--- init.el
+++ init.el
@@ -87,13 +87,15 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(ansi-term-color-vector
+   [unspecified "#2e3440" "#88c0d0" "#bf616a" "#5e81ac" "#ebcb8b" "#a3be8c" "#ebcb8b" "#e5e9f0"])
  '(anzu-deactivate-region t)
  '(anzu-mode-lighter "")
  '(anzu-replace-to-string-separator " => ")
  '(anzu-search-threshold 1000)
  '(custom-safe-themes
    (quote
-    ("31992d4488dba5b28ddb0c16914bf5726dc41588c2b1c1a2fd16516ea92c1d8e" "93268bf5365f22c685550a3cbb8c687a1211e827edc76ce7be3c4bd764054bad" "77a46326228485699b378a8537f9bc5d6b0d087566ac179bec752fab322d814a" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "2e973a84accb627f603f309fc13938daa227055d2a6528c80a9b7a9e2dd8da6b" "b378249b7f647796b186c70f61eaaee7aa1bd123681d5ca8c44d3ca8875e1b70" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "6c35ffc17f8288be4c7866deb7437e8af33cd09930e195738cdfef911ab77274" "77ab33a45a8d39566f24cd6a9b28c78eec89882004ed194587e210399813d009" "aae95fc700f9f7ff70efbc294fc7367376aa9456356ae36ec234751040ed9168" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
+    ("527df6ab42b54d2e5f4eec8b091bd79b2fa9a1da38f5addd297d1c91aa19b616" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "31992d4488dba5b28ddb0c16914bf5726dc41588c2b1c1a2fd16516ea92c1d8e" "93268bf5365f22c685550a3cbb8c687a1211e827edc76ce7be3c4bd764054bad" "77a46326228485699b378a8537f9bc5d6b0d087566ac179bec752fab322d814a" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" "2e973a84accb627f603f309fc13938daa227055d2a6528c80a9b7a9e2dd8da6b" "b378249b7f647796b186c70f61eaaee7aa1bd123681d5ca8c44d3ca8875e1b70" "611e38c2deae6dcda8c5ac9dd903a356c5de5b62477469133c89b2785eb7a14d" "6c35ffc17f8288be4c7866deb7437e8af33cd09930e195738cdfef911ab77274" "77ab33a45a8d39566f24cd6a9b28c78eec89882004ed194587e210399813d009" "aae95fc700f9f7ff70efbc294fc7367376aa9456356ae36ec234751040ed9168" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
  '(fci-rule-color "#383838")
  '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
@@ -107,7 +109,7 @@
  '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (eink-theme base16-theme doom-themes zenburn-theme zerodark-theme yari yaml-mode web-mode volatile-highlights use-package undo-tree ujelly-theme tao-theme spacemacs-theme spaceline-all-the-icons solarized-theme smartrep sly-company scss-mode ruby-tools ruby-end rspec-mode robe rjsx-mode realgud rbenv rainbow-mode rainbow-delimiters pyenv-mode paredit ov operate-on-number nord-theme nlinum move-text meghanada markdown-mode key-chord json-mode irony highlight-symbol helm-projectile helm-descbinds helm-ag gruvbox-theme grizzl google-c-style god-mode gitignore-mode gitconfig-mode git-timemachine gist findr fic-mode expand-region exec-path-from-shell ess emmet-mode elisp-slime-nav easy-kill dracula-theme distinguished-theme discover-my-major dired+ diff-hl darktooth-theme company-tern company-anaconda coffee-mode cider browse-kill-ring avy autodisass-java-bytecode auto-compile anzu all-the-icons-dired alchemist ag)))
+    (base16-theme yari yaml-mode web-mode volatile-highlights use-package undo-tree ujelly-theme tao-theme spacemacs-theme spaceline-all-the-icons solarized-theme smartrep sly-company scss-mode ruby-tools ruby-end rspec-mode robe rjsx-mode realgud rbenv rainbow-mode rainbow-delimiters pyenv-mode paredit ov operate-on-number nord-theme nlinum move-text meghanada markdown-mode key-chord json-mode irony highlight-symbol helm-projectile helm-descbinds helm-ag grizzl google-c-style god-mode gitignore-mode gitconfig-mode git-timemachine gist findr fic-mode expand-region exec-path-from-shell ess emmet-mode elisp-slime-nav easy-kill discover-my-major dired+ diff-hl company-tern company-anaconda coffee-mode cider browse-kill-ring avy autodisass-java-bytecode auto-compile anzu all-the-icons-dired alchemist ag)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(spaceline-all-the-icons-icon-set-modified (quote toggle))
  '(spaceline-all-the-icons-separator-type (quote cup))
